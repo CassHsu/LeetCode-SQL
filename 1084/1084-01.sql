bd5e15592e82f59fa19ceed5c56cb1957203edb3
CassHsu <cass.hsu@gmail.com>
2022-05-12 21:22:44 +0800

    Where NOT IN

---
+++ 1084/1084-01.sql
@@ -0,0 +1,7 @@
+SELECT p.product_id, p.product_name
+FROM Product p
+WHERE p.product_id NOT IN  (
+    SELECT product_id
+    FROM Sales
+    WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
+);
