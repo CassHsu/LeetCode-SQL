69b419556af3381112df01d44d409d4fa6184a05
CassHsu <cass.hsu@gmail.com>
2023-10-03 23:30:41 +0800

    Update 1667-01.sql

--- 1667/1667-01.sql
+++ 1667/1667-01.sql
@@ -1,2 +1,5 @@
-SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
-FROM Users ORDER BY user_id;
+SELECT 
+    user_id,
+    CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id;
